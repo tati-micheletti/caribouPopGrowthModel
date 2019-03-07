720cbafa53e22083ed84def9d3eaa10169c7e991
Tati Micheletti <tati.micheletti@gmail.com>
2019-03-07 18:47:59 +0000

    added title and saving plot to caribou pop growth

--- R/plotCaribou.R
+++ R/plotCaribou.R
@@ -23,7 +23,18 @@ plotCaribou <- function(startTime = start(sim),
   
   plotsCarib <- ggplot2::ggplot(data = populationCaribou, aes(x = Time, 
                                                               y = CaribouPopulationSize)) + 
+    ggtitle("Caribou population dynamics") +
     geom_line()
+  
+  if((currentTime %% 10) == 0){
+    
+    clearPlot()
+    png(file.path(getwd(), "outputs", paste0("caribouPopYear", currentTime, "_", toupper(format(Sys.time(), "%d%b%y")),".png")), 
+        width = 700, height = 480)
+    print(plotsCarib)
+    dev.off()
+    
+  }
   
   return(plotsCarib)
   
