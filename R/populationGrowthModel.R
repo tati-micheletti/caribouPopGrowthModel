66a1d43df6b5b96857ba7085584742982eec0053
Tati Micheletti <tati.micheletti@gmail.com>
2021-05-05 04:07:22 +0000

    Fixed CI calculation, growth interval and wrong cbind

--- R/populationGrowthModel.R
+++ R/populationGrowthModel.R
@@ -2,12 +2,11 @@ populationGrowthModel <-  function(femaleSurvivalModel,
                                    recruitmentModel,
                                    disturbances,
                                    currentTime,
-                                   growthInterval,
+                                   # growthInterval, # Hard coded here until we deal with yearly population
                                    popModel
                                    ){
   
   message(paste0("Forecasting caribou population growth for ", currentTime,"..."))
-  
   covTable <- rbindlist(lapply(names(disturbances), function(shpfls){
     shpTable <- rbindlist(lapply(names(disturbances[[shpfls]]), function(pols){
       DT <- data.table::data.table(disturbances[[shpfls]][[pols]])
@@ -16,28 +15,22 @@ populationGrowthModel <-  function(femaleSurvivalModel,
     }))
     return(shpTable)
   }))
-  polygonsAreas <- covTable[, c("polygon", "area")]
   femaleSurvivalPredictions <- rbindlist(lapply(X = names(femaleSurvivalModel), 
                                       FUN = function(modelType) {
-                                        
     responseList <- generatePopGrowthPredictions(covTable = covTable,
                                                  coeffTable = femaleSurvivalModel[[modelType]][["coeffTable"]],
                                                  coeffValues = femaleSurvivalModel[[modelType]][["coeffValues"]],
                                                  modelType =  modelType,
-                                                 polygonsAreas = polygonsAreas,
                                                  model = "femaleSurevival")
     responseList[, c("modelType", "model") := list(modelType, "femaleSurvival")]
     return(responseList)
   }))
-
   recruitmentPredictions <- rbindlist(lapply(X = names(recruitmentModel), 
                                                 FUN = function(modelType) {
-                                                  
     responseList <- generatePopGrowthPredictions(covTable = covTable, 
                                                  coeffTable = recruitmentModel[[modelType]][["coeffTable"]],
                                                  coeffValues = recruitmentModel[[modelType]][["coeffValues"]],
                                                  modelType =  modelType,
-                                                 polygonsAreas = polygonsAreas,
                                                  model = "recruitment")
     responseList[, c("modelType", "model") := list(modelType, "recruitment")]
     return(responseList)
@@ -49,9 +42,7 @@ populationGrowthModel <-  function(femaleSurvivalModel,
                               recruitmentModel = names(recruitmentModel)))
   
   DT <- rbindlist(lapply(1:NROW(combinations), function(index){
-
-    CI <- function(SD) qnorm(0.975)*SD/sqrt(100) # This dev came from 100 samples
-    
+    CI <- function(SD) qnorm(0.975)*SD
     femSurvMod <- unique(combinations[[index, "femaleSurvivalModel"]])
     recrMod <- unique(combinations[[index, "recruitmentModel"]])
     
@@ -62,37 +53,34 @@ populationGrowthModel <-  function(femaleSurvivalModel,
     recr <- recruitmentPredictions[modelType == recrMod, "average"]
     recrmin <- recr - CI(recruitmentPredictions[modelType == recrMod, "stdErr"])
     recrmax <- recr + CI(recruitmentPredictions[modelType == recrMod, "stdErr"])
-    ts <- growthInterval
+    # ts <- growthInterval
     
     testthat::expect_true(NROW(SadF) == NROW(recr))
     
-    
     growth <- do.call(what = popModel, args = alist(SadF = SadF,
-                                        recr = recr,
-                                        ts = ts))
+                                        recr = recr))
     growthMin <-  do.call(what = popModel, args = alist(SadF = SadFmin,
-                                                      recr = recrmin,
-                                                      ts = ts))
+                                                      recr = recrmin))
     growthMax <-  do.call(what = popModel, args = alist(SadF = SadFmax,
-                                                      recr = recrmax,
-                                                      ts = ts))
+                                                      recr = recrmax))
     
     # Melt DT to put recruitment and femaleSurvival results in the same table 
     DTbind <- rbind(recruitmentPredictions[modelType == recrMod,],
                     femaleSurvivalPredictions[modelType == femSurvMod,])
-    DTbind <- dcast(DTbind, polygon + area ~ model, value.var = c("average", "stdErr"))
-    names(growth) <- "growth"
-    names(growthMax) <- "growthMax"
-    names(growthMin) <- "growthMin"
-    DTbind <- do.call(cbind, args = list(DTbind, growth, growthMin, growthMax))
+    DTbind <- dcast(DTbind, Herd + area ~ model, value.var = c("average", "stdErr"))
+    growthDT <- data.table(annualLambda = growth[["average"]], 
+                           annualLambdaMax = growthMax[["average"]],
+                           annualLambdaMin = growthMin[["average"]],
+                           Herd = femaleSurvivalPredictions$Herd)
+    DTbind <- merge(DTbind, growthDT, by = "Herd")
     DTbind[, femSurvMod_recrMod := paste(femSurvMod, recrMod, sep = "::")]
     return(DTbind)
   }))
   
-  # Adding to annualLambda
-  DT[, c("annualLambda", "annualLambdaMin", "annualLambdaMax") := list(growth^(1/growthInterval), 
-                                                                       growthMin^(1/growthInterval), 
-                                                                       growthMax^(1/growthInterval))]
+  # # Adding to annualLambda  :: Leaving here for backward checks
+  # DT[, c("annualLambda", "annualLambdaMin", "annualLambdaMax") := list(growth,#^(1/growthInterval), 
+  #                                                                      growthMin,#^(1/growthInterval), 
+  #                                                                      growthMax)]#^(1/growthInterval))]
   
   return(DT)
 }
