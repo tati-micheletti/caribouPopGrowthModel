7e540d92d2e0a37b5c670ba2079fcdae2d196a64
Tati Micheletti <tati.micheletti@gmail.com>
2021-05-05 04:05:32 +0000

    Fixed snapshot of lambda -- doesnt need exponentiate as it doesnt depend on previous years

--- R/popModels.R
+++ R/popModels.R
@@ -21,14 +21,23 @@ annualGrowthLogistic <- function(N, SadF, recr){
 
 # Simple lambda model (i.e. realized population growth rate)
 annualLambda <- function(SadF, recr, ...){
+  if (any(recr > 1, recr < 0)){
+    stop("Recruitment needs to be provided per capita (0-1)")
+  }
+  if (any(SadF > 1, SadF < 0)){
+    stop("Female survival needs to be provided per capita (0-1)")
+  }
   mortF <- (1-SadF)*100
-  newL <- (100-mortF)/(100-(100*recr/2)) # basic McLaughlin et al. 2003 lambda model, also used in Sorrensen et al. 2006
+  newL <- (100-mortF)/(100-(100*recr/2))
+  # basic McLaughlin et al. 2003 lambda model, also used in Sorrensen et al. 2006
   return(newL)
 }
 
-timestepLambda <- function(SadF, recr, ts = 10, ...){ 
-  return(annualLambda(SadF, recr, ...) ^ ts)
-}
+# WE NEED TO REVIEW THE CODE BELOW WHEN WE DECIDE TO IMPLEMENT 
+# POPULATION SIZE CALCULATION
+# timestepLambda <- function(SadF, recr, ts = 10, ...){ 
+#   return(annualLambda(SadF, recr, ...) ^ ts)
+# }
 
 # more complicated population model - non-stochastic, but logistic (i.e. density-dependent)
 annualGrowthLogisticLambda <- function(N, SadF, recr, ...){
